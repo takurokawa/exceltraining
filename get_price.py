19588a43b641d40ae7414c55ed8550527df7f411
takurokawa <67994563+takurokawa@users.noreply.github.com>
2021-03-23 14:26:49 +0900

    Update get_price.py

--- get_price.py
+++ get_price.py
@@ -3,7 +3,7 @@ import re
 
 def value_get(base_sheet, search_sheet, id, search_str):
     for row_data in base_sheet.iter_rows(min_row=4):
-        if str(row_data[2].value) == id:
+        if str(row_data[2].value) == str(id):
             base_value = row_data[43].value
             break
 
@@ -13,13 +13,13 @@ def value_get(base_sheet, search_sheet, id, search_str):
                 continue
             elif re.search(r'2021-01-01',str(cell.value)):
                 start_col = cell.column
-            elif re.search(search_str, str(cell.value)):
+            elif search_str == cell.value:
                 end_col = cell.column
                 break
         break
 
     for row_data in search_sheet.iter_rows(min_row=3):
-        if str(row_data[1].value) == id:
+        if str(row_data[1].value) == str(id):
             flag = False
             dec = 0
             for cell in row_data:
@@ -37,40 +37,25 @@ def value_get(base_sheet, search_sheet, id, search_str):
 
 def main():
     target_file = 'FILENAME.xlsx'
+    result_file = 'RESULT.xlsx'
+
     base_value_sheet = 'BASE'
     decrement_value_sheet  = 'SEARCH'
 
-    ids = input('Input searching values:').split()
-    ids.sort()
+    search_wb = openpyxl.load_workbook(result_file)
+    search_wb.save(result_file)
 
-    while True:
-        date_data = input('Input year/month:')
-        if re.fullmatch(r'202[0-9]/([1-9]|10|11|12)',date_data):
-            date_list = date_data.split(sep='/')
-            date_list[1] = date_list[1].zfill(2)
-            search_ = '-'.join(date_list)
-            break
-        else:
-            print('Wrong formant.Please re-enter year/month.\n')
-            continue
-
-    search_ += '-01'
+    sheet_list = search_wb.sheetnames
+    ws = search_wb[sheet_list[0]]
+    search_date = ws['C2'].value
 
     wb = openpyxl.load_workbook(target_file)
-    result_list = [['ID','TOTAL']]
-    for id in ids:
-        result_list.append([id, value_get(wb[base_value_sheet], wb[decrement_value_sheet], id, search_)])
 
-    new_wb = openpyxl.Workbook()
-    ws = new_wb.active
+    for row_data in ws.iter_rows(min_row=5,min_col=2):
+        row_data[1].value = value_get(wb[base_value_sheet], wb[decrement_value_sheet], row_data[0].value, search_date)
 
-    for y, row in enumerate(result_list):
-        for x, cell in enumerate(row):
-            ws.cell(row=y+1,
-                    column=x+1,
-                    value=result_list[y][x])
 
-    new_wb.save('result.xlsx')
+    search_wb.save(result_file)
 
 if __name__ == '__main__':
     main()
